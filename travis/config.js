9f2ab9a333c9cd99adf7ac9d229b0ff8dba87f3c
powerpuffgirlschx <37470609+powerpuffgirlschx@users.noreply.github.com>
2018-03-17 21:20:14 +0530

    added files

---
+++ travis/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAKftDQOAmQBAM2fvSsEqxATYZAtczwbho7QZCWmrDlUP6iO9PClgrR2m6dBnUi3eu3DF52XKIhPKsvx6hhc2bDfwKwn1ZAPZAwYZB09yOydLuQhlgofmXZBuPKziaBSnjh9WKE1les8hrejMWqCL0ZB80O0PsyYTPqZBIsan9DEtqdQkv8ZBQb1OH3QRW4w8ZCXwZD',
+    FB_VERIFY_TOKEN: 'chemical_X1',
+    API_AI_CLIENT_ACCESS_TOKEN: 'ef19f53b67a54f5cadae068fc862103f',
+    FB_APP_SECRET: 'c3106be5cf6711bc5445d881a39c2e63',
+    SERVER_URL: "https://travis-rajasthan.herokuapp.com/",
+    WEATHER_API_KEY: 'f3b7c5d36b057d9f597d6aac72d07be6',
+};
